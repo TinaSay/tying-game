e0c35741f573cc6281b65ca4d00c02b265507414
TinaSayapina <tina.sayapina@toptal.com>
2023-09-14 13:40:07 +0500

    Formatted code

--- script.js
+++ script.js
@@ -1,67 +1,66 @@
-const quoteApiUrl='https://api.quotable.io/random';
-const quoteText=document.getElementById('quote')
-const textArea=document.getElementById('quoteInput')
-const counter=document.querySelector('.counter')
+const quoteApiUrl = 'https://api.quotable.io/random';
+const quoteText = document.getElementById('quote')
+const textArea = document.getElementById('quoteInput')
+const counter = document.querySelector('.counter')
 
 
-const fetchQuote=()=>{
+const fetchQuote = () => {
     return fetch(quoteApiUrl)
-        .then(response=>response.json())
-        .then(data=>data.content)
+        .then(response => response.json())
+        .then(data => data.content)
 }
 
-async function renderQuote(){
+async function renderQuote() {
     const quote = await fetchQuote();
-    quoteText.innerHTML=""
-    quote.split('').forEach((el,index)=>{
-        const span=document.createElement('span')
-        span.innerText=el;
+    quoteText.innerHTML = ""
+    quote.split('').forEach((el, index) => {
+        const span = document.createElement('span')
+        span.innerText = el;
         quoteText.appendChild(span)
         span.classList.remove('incorrect')
     })
-    textArea.value=null;
+    textArea.value = null;
     startTimer();
 }
+
 renderQuote()
 let startTime;
 
-const startTimer=()=>{
-    startTime=new Date();
+const startTimer = () => {
+    startTime = new Date();
 
-    setInterval(()=>{
-       counter.innerText = setTimerTime()
+    setInterval(() => {
+        counter.innerText = setTimerTime()
 
-    },1000)
+    }, 1000)
 }
 
-const setTimerTime=()=>{
-    return Math.floor((new Date()-startTime)/1000)
+const setTimerTime = () => {
+    return Math.floor((new Date() - startTime) / 1000)
 
 }
 
-textArea.addEventListener('input',(e)=>{
-    const spans=document.querySelectorAll('span')
-    const inputLetters=textArea.value.split('')
-    let correct=true;
+textArea.addEventListener('input', (e) => {
+    const spans = document.querySelectorAll('span')
+    const inputLetters = textArea.value.split('')
+    let correct = true;
 
-    spans.forEach((el,index)=>{
-        const character=inputLetters[index];
-        if (character==null){
+    spans.forEach((el, index) => {
+        const character = inputLetters[index];
+        if (character == null) {
             el.classList.remove('correct')
             el.classList.remove('incorrect')
-            correct=false;
-        }
-        else if (character===el.innerText){
+            correct = false;
+        } else if (character === el.innerText) {
             el.classList.add('correct')
             el.classList.remove('incorrect')
-        }
-        else {
+        } else {
             el.classList.add('incorrect')
             el.classList.remove('correct')
-            correct=false
+            correct = false
         }
     })
-    if(correct) renderQuote();
+    if (correct) renderQuote();
 
 })
 
